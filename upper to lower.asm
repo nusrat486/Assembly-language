4eba421e58ae3d24980abf214960a5d66bebdb22
nusrat486 <nusrat.cse2022cstu@gmail.com>
2026-05-18 09:41:32 +0600

    Add files via upload

---
+++ upper to lower.asm
@@ -0,0 +1,31 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+.model small
+.stack 100h
+.data
+msg db "Enter upper case letter:$"
+.code
+
+main proc
+    mov ax,@data
+    mov ds,ax
+    
+    mov ah,1
+    int 21h
+    mov bl,al
+    
+    add bl,32
+    mov ah,2
+    mov dl,bl
+    int 21h
+    
+
+ret
+
+
+
+
