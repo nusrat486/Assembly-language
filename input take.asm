fdc8437d91a19bf517a8f3cc6492b2847d729d3d
nusrat486 <nusrat.cse2022cstu@gmail.com>
2026-05-18 09:40:49 +0600

    Add files via upload

---
+++ input take.asm
@@ -0,0 +1,39 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+
+
+.model small
+.stack 100h
+.code
+
+main proc
+    mov ah,1
+    int 21h
+    mov bl,al
+    
+    mov ah,1
+    int 21h
+    mov bh,al
+    
+    mov ah,2
+    mov dl,bl
+    int 21h
+    
+    mov ah,2
+    mov dl,bh
+    int 21h
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
+
+
+
+
+
+
+
